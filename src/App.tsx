18ae6a5e82af7518121f0c99f26b58dd9c186866
Otávio <otavio@leanonsystems.com>
2021-11-04 19:30:00 -0300

    grid implemented, immer added to yarn pkg

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import produce from 'immer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const nRows = 40;
+const nColumns = 50;
+
+const App: React.FC = () => {
+	const [grid, setGrid] = useState(() => {
+		const rows = [];
+		for (let i = 0; i < nRows; i++) {
+			rows.push(Array.from(Array(nColumns), () => 0));
+		}
+
+		return rows;
+	});
+
+	return <div style={{
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: '1%'
+	}}>
+
+		<div style={{
+			display: 'grid',
+			gridTemplateColumns: `repeat(${nColumns}, 20px)`
+		}}>
+			{grid.map((rows, i) => rows.map((col, j) => <div
+				key={`${i}-${j}`}
+				onClick={() => {
+					const newGrid = produce(grid, gridCopy => {
+						gridCopy[i][j] = grid[i][j] === 1 ? 0 : 1;
+					})
+					setGrid(newGrid)
+				}}
+				style={{
+					width: 20,
+					height: 20,
+					backgroundColor: grid[i][j] ? '#5e99c5' : undefined,
+					border: 'solid 1px #979797'
+				}} />)
+			)}
+		</div>
+	</div>;
 }
 
 export default App;
